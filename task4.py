9c178192ef8892e5a121d38d0d13ddc7e822ac0d
rutvik2611 <rutvik2611@gmail.com>
2021-03-29 12:41:19 -0400

    TASK 13 AND 14

--- task4.py
+++ task4.py
@@ -135,3 +135,15 @@ def fun():
 
 print(fun())
 
+#q13
+
+from functools import reduce
+
+y=reduce(lambda a,b:10*a+b,[1,2,3,4,5,6,7])
+print(y)
+
+#q14
+
+n=int(input("Enter the value of n: "))
+x=filter(lambda x:x%3!=0 and x%7==0,range(1,n+1))
+print(list(x))
